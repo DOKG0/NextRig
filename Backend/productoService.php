d716970c81ad13e11717b8c60c7fbe23ab598e6f
DOKG0 <agustin-26-4-1996@hotmail.com>
2025-04-25 17:28:22 -0300

    feat: Listar productos

--- Backend/productoService.php
+++ Backend/productoService.php
@@ -6,5 +6,37 @@ require_once('config.php');
             $database = new Database();
             $this->db_conn = $database->getConnection();
         }
+
+        public function getComponentsByCategory($categoria){
+            $query = "SELECT * FROM Componentes JOIN Productos ON Componentes.id = Productos.id WHERE Componentes.categoria = '$categoria'";
+            $resultado = mysqli_query($this->db_conn, $query);
+            if (!$resultado) {
+                http_response_code(500);
+                echo json_encode(["error" => "Error en la consulta a la base de datos."]);
+                exit;
+            }
+            $componentes = [];
+            while ($fila = mysqli_fetch_object($resultado)) {
+                $componentes[] = $fila;
+            }
+    
+            return $componentes;
+        }
+
+        public function listarProductos() {
+            $query = "SELECT * FROM Productos";
+            $resultado = mysqli_query($this->db_conn, $query);
+            if (!$resultado) {
+                http_response_code(500);
+                echo json_encode(["error" => "Error en la consulta a la base de datos."]);
+                exit;
+            }
+    
+            $productos = [];
+            while ($fila = mysqli_fetch_object($resultado)) {
+                $productos[] = $fila;
+            }
+            return $productos;
+        }
     }
 ?>
