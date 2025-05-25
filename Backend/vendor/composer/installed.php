4d5e48efd2008604b015fb1522bee2d3ac95adbf
DOKG0 <agustin-26-4-1996@hotmail.com>
2025-05-25 15:40:46 -0300

    feat: Se agrega phpmailer al form de contacto y se modifican estilos

---
+++ Backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dokgo/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '06a7b715afbf7823b5fc51ff0f5fdf630c69cf92',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dokgo/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '06a7b715afbf7823b5fc51ff0f5fdf630c69cf92',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
